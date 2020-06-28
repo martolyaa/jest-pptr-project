196fbb56781e3a54ec03c6aaf5e0f5601236757d
martolyaa <37909601+martolyaa@users.noreply.github.com>
2020-06-28 22:22:02 +0300

    Framework part4 FeedbackForm

--- tests/example.test.js
+++ tests/example.test.js
@@ -1,17 +1,18 @@
 import HomePage from "../pages/HomePage";
 import TopBar from "../pages/components/TopBar";
 import LoginPage from "../pages/LoginPage";
+import FeedbackPage from "../pages/FeedbackPage";
 
 describe('Example', () => {
     let homepage;
-    let loginpage;
+    let feedbackPage;
     let topbar;
 
     beforeAll(async () => {
         jest.setTimeout(15000);
         homepage = new HomePage();
-        loginpage = new LoginPage();
-        topbar = new TopBar()
+        feedbackPage = new FeedbackPage();
+        topbar = new TopBar();
     });
 
     it("homepage should work", async () => {
@@ -23,10 +24,15 @@ describe('Example', () => {
         await topbar.isTopBarDisplayed();
     });
 
-    it("try to login", async () => {
-        await loginpage.visit();
-        await loginpage.isLoginFormDisplayed();
-        await loginpage.login("username", "password");
-        await loginpage.wait(5000);
-    })
+    it("feedback should work", async () => {
+        await feedbackPage.visit();
+        await feedbackPage.isFeedbackFormDisplayed();
+        await feedbackPage.submitFeedback(
+            "Johny", 
+            "johnyTheSales@gmail.com", 
+            "subject", 
+            "comment"
+            );
+        await feedbackPage.wait(5000);
+    });
 });
